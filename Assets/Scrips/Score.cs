b73eaa837851dc11480ef03e2f43888558f94594
TuckoxXL <sebastianbelluccini@gmail.com>
2023-03-01 17:03:40 -0300

    actualización 2.1
    Mejora del Score

--- Assets/Scrips/Score.cs
+++ Assets/Scrips/Score.cs
@@ -6,43 +6,20 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Text HiScoreText;
-    int score;
-    Text scoreText;
+    public Text scoreText;
+    public float scorenumero;
+    public float scorSuma;
 
-    float Timer;
-    float maxTimer;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        HiScoreText.text = "Puntaje Maximo " + PlayerPrefs.GetInt("highscore", 0).ToString("000000");
-        score = 0;
-        scoreText = GetComponent<Text>();
-        maxTimer = 0.1f;
-        
+        scorenumero = 0f;
+        scorSuma = 10f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Timer += Time.deltaTime;
-        if(Timer >= maxTimer)
-        {
-            score++;
-            string v = score.ToString("000000");
-            scoreText.text = v;
-            Timer = 0;
-        }
 
-        if (Time.timeScale == 0)
-        {
-            if (score > PlayerPrefs.GetInt("highscore", 0))
-            {
-                PlayerPrefs.SetInt("highscore", score);
-                HiScoreText.text = "Puntaje Maximo " + PlayerPrefs.GetInt("highscore", 0).ToString("000000");
-            }
-        }
+    private void Update()
+    {
+        scoreText.text = "Score:" + (int)scorenumero;
+        scorenumero += scorSuma * Time.deltaTime;
     }
-    
 }
